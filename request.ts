95c9117917859c254eb71d87b40934bed82ba998
Michael Spengler <mycamper@gmx.de>
2020-08-04 11:24:41 +0200

    improving flexibility for user

--- request.ts
+++ request.ts
@@ -1,7 +1,17 @@
+import { IOptions, defaultOptions } from "./interfaces-and-constants.ts"
+
 export class Request {
 
-    static async get(url: string): Promise<any> {
-        let response: any = await fetch(url)
+    static async get(url: string, options: IOptions = defaultOptions): Promise<any> {
+        let response: any = await fetch(url, {
+            method: 'GET',
+            mode: options.mode,
+            cache: options.cache,
+            credentials: options.credentials,
+            headers: options.headers,
+            redirect: options.redirect,
+            referrerPolicy: options.referrerPolicy,
+        })
 
         let result = await response.json()
 
@@ -9,29 +19,33 @@ export class Request {
     }
 
 
-    static async post(url: string, data: any): Promise<any> {
+    static async post(url: string, data: any, options: IOptions = defaultOptions): Promise<any> {
         const response = await fetch(url, {
             method: 'POST',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer',
+            mode: options.mode,
+            cache: options.cache,
+            credentials: options.credentials,
+            headers: options.headers,
+            redirect: options.redirect,
+            referrerPolicy: options.referrerPolicy,
             body: JSON.stringify(data)
         })
-        
+
         let result: any = await response.json()
 
         return result
     }
 
 
-    static async delete(url: string): Promise<any> {
+    static async delete(url: string, options: IOptions = defaultOptions): Promise<any> {
         let response: any = await fetch(url, {
             method: 'DELETE',
+            mode: options.mode,
+            cache: options.cache,
+            credentials: options.credentials,
+            headers: options.headers,
+            redirect: options.redirect,
+            referrerPolicy: options.referrerPolicy,
         })
 
         let result: any = await response.json()
@@ -40,17 +54,15 @@ export class Request {
     }
 
 
-    static async put(url: string, data: any): Promise<any> {
+    static async put(url: string, data: any, options: IOptions = defaultOptions): Promise<any> {
         const response: any = await fetch(url, {
             method: 'PUT',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer',
+            mode: options.mode,
+            cache: options.cache,
+            credentials: options.credentials,
+            headers: options.headers,
+            redirect: options.redirect,
+            referrerPolicy: options.referrerPolicy,
             body: JSON.stringify(data)
         })
 
