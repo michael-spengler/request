7f1412281e713fcc6041e0e9fb18705241e87ad1
oganexon <oganexon@protonmail.com>
2021-08-26 16:59:26 +0200

    feat: enhance request module

--- mod.ts
+++ mod.ts
@@ -1,2 +1,52 @@
-export * from "./interfaces-and-constants.ts"
-export * from "./request.ts"
+export const defaultOptions: RequestInit = {
+  mode: "cors",
+  cache: "no-cache",
+  credentials: "same-origin",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  redirect: "follow",
+  referrerPolicy: "no-referrer",
+};
+
+export class RequestError extends Error {
+  status: number;
+  statusText: string;
+
+  constructor(message: string, response: Response) {
+    super(`${message}: ${response.status} ${response.statusText}`);
+    this.status = response.status;
+    this.statusText = response.statusText;
+  }
+}
+
+export function request(method: string) {
+  return async function <T>(
+    input: string | globalThis.Request | URL,
+    init?: RequestInit | undefined,
+  ): Promise<T> {
+    const response = await fetch(input, {
+      method,
+      ...defaultOptions,
+      ...init,
+    });
+    if (response.ok) {
+      return response.json();
+    }
+    throw new RequestError(`Error while fetching ${input}`, response);
+  };
+}
+
+export const Request = {
+  get: request("GET"),
+  head: request("HEAD"),
+  post: request("POST"),
+  put: request("PUT"),
+  delete: request("DELETE"),
+  connect: request("CONNECT"),
+  options: request("OPTIONS"),
+  trace: request("TRACE"),
+  patch: request("PATCH"),
+};
+
+export default Request;
